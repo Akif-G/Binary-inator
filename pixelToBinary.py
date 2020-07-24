066d66075d9810037924af93934fdba20b0bc331
Akif Gultekin <mehmetgultekin@sabanciuniv.edu>
2020-07-24 05:11:52 +0300

    threshold is now brigthness adaptivegit status very good improvement

--- pixelToBinary.py
+++ pixelToBinary.py
@@ -21,8 +21,15 @@ def pixelToBinary(text):
 
     grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
 
-    (thresh, blackAndWhiteImage) = cv2.threshold(
-        grayImage, 127, 255, cv2.THRESH_BINARY)
+    # too dark or too brigth
+    threshold = 127
+    while (threshold > 75 and threshold < 180) and (blackAndWhiteImage.sum() < 102000 or blackAndWhiteImage.sum() > 306000):
+        (thresh, blackAndWhiteImage) = cv2.threshold(
+            grayImage, threshold, 255, cv2.THRESH_BINARY)
+        if blackAndWhiteImage.sum() < 102000:
+            threshold -= 1
+        elif blackAndWhiteImage.sum() > 306000:
+            threshold += 1
 
     rows, cols = blackAndWhiteImage.shape
     for i in range(rows):
